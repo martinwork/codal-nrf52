88cf02ddb4f7fd423f613eb7b6b9bafd9cc42ff4
Martin Williams <martinwork@users.noreply.github.com>
2025-04-30 20:09:18 +0100

    Prevent IRQ clashing with fiber access to buffers

--- source/NRF52PWM.cpp
+++ source/NRF52PWM.cpp
@@ -41,6 +41,7 @@ NRF52PWM::NRF52PWM(NRF_PWM_Type *module, DataSource &source, float sampleRate, u
     this->repeatOnEmpty = true;
     this->bufferPlaying = 0;
     this->stopStreamingAfterBuf = 0;
+    this->inPullRequest = false;
 
     // Clear empty buffer
     for (int i=0; i<NRF52PWM_EMPTY_BUFFERSIZE; i++)
@@ -248,7 +249,6 @@ int NRF52PWM::tryPull(uint8_t b)
         return 0;
     }
 
-    if (dataReady){
         buffer[b] = upstream.pull();
         PWM.SEQ[b].PTR = (uint32_t) buffer[b].getBytes();
         PWM.SEQ[b].CNT = buffer[b].length() / 2;
@@ -297,6 +297,7 @@ int NRF52PWM::pullRequest()
     if (streaming && !active)
     {
         active = true;
+        inPullRequest = true;
 
         tryPull(bufferPlaying);
         bufferPlaying = (bufferPlaying + 1) % 2;
@@ -306,6 +307,8 @@ int NRF52PWM::pullRequest()
             tryPull(bufferPlaying);
             bufferPlaying = (bufferPlaying + 1) % 2;
         }
+        
+        inPullRequest = false;
 
         // Check if we've preloaded both buffers
         if (bufferPlaying == 0)
@@ -322,20 +325,25 @@ int NRF52PWM::pullRequest()
  */
 void NRF52PWM::irq()
 {
+    
     // once the sequence has finished playing, load up the next buffer.
     if (PWM.EVENTS_SEQEND[0])
     {
-        bufferPlaying = 1;
-        tryPull(0);
-
+        if ( !inPullRequest)
+        {
+            bufferPlaying = 1;
+            tryPull(0);
+        }
         PWM.EVENTS_SEQEND[0] = 0;
     }
 
     if (PWM.EVENTS_SEQEND[1])
     {
-        bufferPlaying = 0;
-        tryPull(1);
-
+        if ( !inPullRequest)
+        {
+            bufferPlaying = 0;
+            tryPull(1);
+        }
         PWM.EVENTS_SEQEND[1] = 0;
     }
 }
